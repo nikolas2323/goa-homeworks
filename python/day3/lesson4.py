4e36954028124e47a795b84955f3c5a0221905a5
nikolas2323 <nikushjabua@gmail.com>
2024-02-19 19:01:33 +0400

    third commit

---
+++ python/day3/lesson4.py
@@ -0,0 +1,6 @@
+name = "Niko"
+surname = "Jabua,and"
+age = 12
+in_ten_years = (age + 10)
+
+print("Hello my name is" +" " +name + " "+ surname+" "+ str(age) +" " "in ten years I'll be" " " + str(in_ten_years))
