d277fc906e94b53a285a18b8be1111fd2d09ed85
nikolas2323 <nikushjabua@gmail.com>
2024-03-28 21:26:53 +0400

    commit

--- day6/homework/lesson9.py
+++
@@ -1,3 +0,0 @@
-age=int(input("Enter your age: "))
-
-print(age * 7 // 3 *(4*9))
--- day8/classwork/class.py
+++
@@ -1,9 +0,0 @@
-#x = (input("enter your name:"))
-#y = (input("enter your name:"))
-
-#print(type(x), type(y))
-
-a = "note"
-b = "book"
-
-print(a+b)
