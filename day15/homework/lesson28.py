839a32e803ef4c112627e55ece525125563f6481
nikolas2323 <nikushjabua@gmail.com>
2024-04-03 19:05:32 +0400

    commit

---
+++ day15/homework/lesson28.py
@@ -0,0 +1,14 @@
+grade=int(input("Enter your grade:"))
+
+
+if grade>=90 and grade<=100:
+    print("Your studies will be fully funded")
+
+elif grade>=70 and grade<=80:
+        print("You will be financed with 1500 GEL")
+
+if grade>=40 and grade<=70:
+            print("You will be financed with 500 GEL")
+
+elif grade<40:
+        print("Your studies will not be financed")
---
+++ day15/homework/lesson29.py
@@ -0,0 +1,12 @@
+grade=int(input("Enter your grade:"))
+if grade==10:
+    print("Your child is a genious!")
+
+elif grade==8 or grade==9:
+    print("Your child is smart but sometimes they zone out")
+
+if grade==5:
+    print("Your child doesn't listen at all")
+
+elif grade<5:
+    print("Your child has been expelled!")
