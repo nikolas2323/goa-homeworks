dde0733d79d951590f6d4fb25fbbaf2b0a4fe47b
nikolas2323 <nikushjabua@gmail.com>
2024-04-06 20:25:44 +0400

    commit

--- day15/homework/lesson28.py
+++ day15/homework/lesson28.py
@@ -2,13 +2,12 @@ grade=int(input("Enter your grade:"))
 
 
 if grade>=90 and grade<=100:
-    print("Your studies will be fully funded")
+    print("თქვენ დაგიფინანსდებათ სწავლა სრულიად")
 
 elif grade>=70 and grade<=80:
-        print("You will be financed with 1500 GEL")
-
+        print("თქვენ დაგიფინანსდებათ სწავლა 1500 ლარით")
 if grade>=40 and grade<=70:
-            print("You will be financed with 500 GEL")
+            print("თქვენ დაგიფინანსდებათ სწავლა 500 ლარით")
 
 elif grade<40:
-        print("Your studies will not be financed")
+        print("თქვენ არ დაგიფინანსდებათ სწავლის პროცესი")
