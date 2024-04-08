78bbac7cb5ceee9381feb957025f3826aaf5cfb2
nikolas2323 <nikushjabua@gmail.com>
2024-04-08 19:28:22 +0400

    'commit'

---
+++ day17/classwork/index.py
@@ -0,0 +1,16 @@
+# products=["milk","bread",True]
+
+
+# print(products[0])
+# print(products[2])
+
+
+# for i in 'banana':
+#     print(i)
+
+
+nums=[3,4,5,7,53,452,5235]
+
+for i in nums:
+    print(i)
+
---
+++ day17/homework/lesson32.py
@@ -0,0 +1,7 @@
+list=["dog","GOA","Niko","hackaton"]
+
+x=list[0]="GOA is the best!"
+z=list[1]="you should join goa"
+y=list[2]="you will learn so much!"
+c=list[3]="Also dogs are the best!"
+print(x+" "+z+" "+y+" "+c)
